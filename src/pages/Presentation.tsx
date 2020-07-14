ec0dc242fc3acf0cb64a51c8873008ed585da8ab
shellbear <shellbear@protonmail.com>
2020-07-14 15:31:36 +0200

    Fix newsletter form

--- src/pages/Presentation.tsx
+++ src/pages/Presentation.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { Box, Heading, Paragraph, Form, FormField, Button } from 'grommet';
+import { Box, Heading, Paragraph, FormField, Button, TextInput } from 'grommet';
 
-export const Presentation: React.FC = () => {
-  const [submitted, setSubmitted] = React.useState(false);
-  const onSubmit = (evt: any) => {
-    const values = Object.entries(evt.value).map(e => e.join('=')).join('&');
 
-    evt.preventDefault();
-    return fetch('https://hiddentity.us4.list-manage.com/subscribe/post?u=2a2d72067208921b3df7a14f4&amp;id=f28c53000a', {
-      method: 'POST',
-      body: new URLSearchParams(values),
-    }).finally(() => setSubmitted(true));
-  };
 
-  return (
+export const Presentation: React.FC = () => (
     <Box
       fill='horizontal'
       direction='row'
@@ -24,37 +14,24 @@ export const Presentation: React.FC = () => {
       pad='xlarge'
       wrap
     >
-      <Box width='large'>
+      <Box width='large' pad='xsmall'>
         <Heading>Trouvez l'emploi que vous méritez vraiment</Heading>
         <Paragraph>Notre mission : supprimer les barrières du recrutement.</Paragraph>
       </Box>
       <Box background='white' width='medium' round pad='medium'>
         <Heading level='3'>Intéressé(e) par le projet ?</Heading>
-        {submitted ? (
-          <Box alignSelf='center'>
-            <Paragraph>
-              Merci beaucoup pour l'intérêt que vous portez au projet !
-              Nous vous tiendrons informé(e) de l'avancée du projet.
-            </Paragraph>
-            <Heading level='5'>
-              Toute l'équipe Hiddentity&nbsp;
-              <span role='img' aria-label='red-heart'>❤️</span></Heading>
-          </Box>
-        ) :(
-          <>
-            <Paragraph>
-              Car ce n'est qu'ensemble que nous pourrons construire l'avenir du recrutement&nbsp;
-              <span role='img' aria-label='raised-hand'>✋</span>
-            </Paragraph>
-            <Form onSubmit={onSubmit}>
-              <FormField name='EMAIL' type='email' placeholder='me@hiddentity.fr' required/>
-              <Button primary type='submit' label='Suivre le projet'/>
-            </Form>
-          </>
-        )}
+          <Paragraph>
+            Car ce n'est qu'ensemble que nous pourrons construire l'avenir du recrutement&nbsp;
+            <span role='img' aria-label='raised-hand'>✋</span>
+          </Paragraph>
+          <form action='https://hiddentity.us4.list-manage.com/subscribe/post?u=2a2d72067208921b3df7a14f4&amp;id=f28c53000a' method='post'>
+            <FormField>
+              <TextInput name='EMAIL' type='email' placeholder='me@hiddentity.fr' required />
+            </FormField>
+            <Button primary type='submit' label='Suivre le projet' />
+          </form>
       </Box>
     </Box>
   );
-};
 
 export default Presentation;
